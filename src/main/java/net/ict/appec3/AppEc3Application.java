aae35837bf4085641d91c739494dfb1fe51399f4
yumi <mycaptain0771@gmail.com>
2022-12-05 12:00:12 +0900

    EC2go

---
+++ src/main/java/net/ict/appec3/AppEc3Application.java
@@ -0,0 +1,13 @@
+package net.ict.appec3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEc3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppEc3Application.class, args);
+    }
+
+}
---
+++ src/test/java/net/ict/appec3/AppEc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ict.appec3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEc3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
